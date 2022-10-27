080a282049c5ba7564705266fde04eec967ed024
SalmanKarimUSA <98289878+SalmanKarimUSA@users.noreply.github.com>
2022-10-27 13:15:13 -0400

    Update HomePageTests.java

--- amarr/src/test/java/amarr/homepagetests/HomePageTests.java
+++ amarr/src/test/java/amarr/homepagetests/HomePageTests.java
@@ -1,5 +1,8 @@
 package amarr.homepagetests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import amarr.core.ElementFetcher;
@@ -12,7 +15,7 @@ public class HomePageTests extends BaseTests {
 	ElementFetcher fetcher= new ElementFetcher();
 	
 	
-	@Test(testName = "Amarr Logo verification tests")
+	@Test(testName = "Amarr Logo and url verification tests")
 	public void tc_1() {
 		
 		HomePageEvents home=new HomePageEvents();
@@ -21,26 +24,53 @@ public class HomePageTests extends BaseTests {
 		boolean isPresent=home.amarrLogoVerification();
 		
 		fetcher.verify(isPresent, true, "Logo is present");
+	 System.out.println("We are currently on the following URL" +driver.getCurrentUrl());
 	}
+	
 	
 	
 	@Test(testName = "Login with valid id ")
 	public void tc_2() {
+	
+     HomePageEvents home=new HomePageEvents();
 		
-	//TO DO
+		home.clickAcceptCookies();
+		LoginPageEvents login = home.clickOnIAmADealer();
+		login.enterEmailId("salmankarim@assaabloy.com");
+		login.clickOnContinue();
+		System.out.println("We are currently on the following URL" +driver.getCurrentUrl());
+		
+	}
+	
+	@Test(testName = "Login with invalid id ")
+	public void tc_3() {
+	
+     HomePageEvents home=new HomePageEvents();
+		
+        home.clickAcceptCookies();
+		LoginPageEvents login = home.clickOnIAmADealer();
+		login.enterEmailId("salmankarim@assaabloy.com");
+		login.clickOnContinue();
+		
+		
+	
 	}
 	
 	
-	
-	
+	@Test(testName = "login with an invalid credential")
+	public void tc_4() {
+		HomePageEvents home = new HomePageEvents();
+		home.clickRejectCookies();
+		LoginPageEvents login = home.clickOnIAmADealer();
+		login.enterEmailId("abc@nomail.com");
+		login.clickOnContinue();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.findElement(By.id("password")).sendKeys("XYZ@123");
+		driver.findElement(By.xpath("//button[@name='action']")).click();
+		if(driver.getPageSource().contains("Wrong email or password"))
+		{System.out.println("Wrong email or password");}
+		else {System.out.println("Text not present");}
+	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
 }
