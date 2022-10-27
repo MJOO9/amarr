d0d0535776d26115504225673c0484dce674299d
SalmanKarimUSA <98289878+SalmanKarimUSA@users.noreply.github.com>
2022-10-27 17:20:21 -0400

    Update HomePageEvents.java

--- amarr/src/main/java/amarr/pageevents/HomePageEvents.java
+++ amarr/src/main/java/amarr/pageevents/HomePageEvents.java
@@ -30,7 +30,25 @@ public class HomePageEvents implements HomePageElements{
 		fetcher.getWebElement("xpath", iAmADealerXpath).click();
 		return new LoginPageEvents();
 	}
-	public boolean amarrLogoVerification() {
+	public boolean garageDoorsLinkVerify() {
 	 return	fetcher.doesElementExists("xpath", amarrLogoXpath);
 	}
+	
+	public boolean amarrLogoVerification() {
+		 return	fetcher.doesElementExists("xpath", garageDoorXpath);
+		}
+	public boolean commercialDoorsLinkVerify() {
+		 return	fetcher.doesElementExists("xpath", commercialDoorXpath);
+		}
+	public boolean designGarageDoorLinkVerify() {
+		 return	fetcher.doesElementExists("xpath", designYourGarageDoorXpath);
+		}
+	public boolean whereToBuyLinkVerify() {
+		 return	fetcher.doesElementExists("xpath", whereToBuyXpath);
+		}
+	public boolean supportLinkVerify() {
+		 return	fetcher.doesElementExists("xpath", supportXpath);
+		}
+	
+	
 }
